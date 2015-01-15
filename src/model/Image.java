9f0465c3591a62a6c5ef024500ccb88602e9a77f
Familia <Familia@192.168.1.12>
2015-01-15 18:38:28 +0000

    - Creando paquetes - Implementación del modelo

---
+++ src/model/Image.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Image {
+    
+    public Bitmap getBitmap();
+    public Image getNext();
+    public Image getPrev();
+    public void setNext(Image image);
+    public void setPrev(Image image);
+    
+}
